99b25f9aa50b4f21233802096cee158d022177bb
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-07-07 19:45:29 -0600

    add solutions to 15 and 16 from assignment

--- variables.rb
+++ variables.rb
@@ -7,17 +7,29 @@ two = 2
 buckle = 3
 shoe = 4
 
-11
+# 11
 flot = 1.22
 flowt = 2.22
 flowet = 2.34
 
-12 
+# 12  
 commit
 
-13. booleans
+# 13. booleans
 is_it? = true
 
 negation = false
 
-14. commit
+# 14. commit
+
+# 15. mutate string variables
+str = "not the same as before"
+longer_str = "hey bruh, why'd you change the string?"
+
+# 16. puts
+
+puts "ermahgerd"
+puts "mehrner"
+puts "gibberish is a valid language"
+
+# 17. 
